78066b74b0f8d5357b963047c8cf68e95e5e0a55
AlexDeng <a.denisov@elesta.ru>
2022-04-18 15:47:26 +0300

    Firebase connect

---
+++ src/base.js
@@ -0,0 +1,13 @@
+import reBase from "re-base";
+import firebase from "firebase";
+
+const firebaseApp = firebase.initializeApp({
+    apiKey: "AIzaSyC7FheKXY1NOHZ9M9h7DHmuvhSK8gN1-mE",
+    authDomain: "burger-shop-849d4.firebaseapp.com",
+    databaseURL: "https://burger-shop-849d4-default-rtdb.europe-west1.firebasedatabase.app",
+})
+
+const base = reBase.createClass(firebaseApp.database())
+
+export { firebaseApp };
+export default base;
--- src/components/order.js
+++ src/components/order.js
@@ -1,6 +1,25 @@
 import React from 'react';
+import Shipment from './shipment';
 
 class Order extends React.Component {
+  renderOrder = key => {
+    const burger = this.props.burgers[key];
+    const count = this.props.order[key];
+
+    const isAvailable = burger && burger.status === 'available';
+    if (!isAvailable) {
+      return <li className='unavailable' key={key}>
+        Извините, {burger ? burger.name : 'бургер'} временно недоступен
+      </li>
+    }
+    return <li key={key}>
+      <span>
+        <span>{count}</span>
+        шт. {burger.name}
+        <span> {count * burger.price} ₽</span>
+        <button className='cancelItem'>&times;</button>
+      </span></li>
+  }
   render() {
     const orderIds = Object.keys(this.props.order);
     const total = orderIds.reduce((prevTotal, key) => {
@@ -18,10 +37,17 @@ class Order extends React.Component {
       <div className='order-wrap'>
         <h2>Ваш заказ</h2>
         <ul className='order'>
-          {orderIds.map(key => {
-            return <li>{key}</li>
-          })}</ul>
-        {total}
+          {orderIds.map(this.renderOrder)}</ul>
+        {total > 0 ? (
+          <Shipment total={total} />
+        ) : (
+          <div className='nothingSelected'>
+            Выберите блюда и добавьте к заказу
+          </div>
+        )
+        }
+
+
       </div>
     )
   }
